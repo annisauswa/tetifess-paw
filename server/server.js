466fe1ccaa82fd6044f5105c019d393ead9ec492
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-09-24 06:28:07 +0700

    feat: welcome message on root dir

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const mongoose = require('mongoose')
 const user = require('./routes/user')
+const posting = require('./routes/posting')
 
 const app = express()
 
@@ -15,12 +16,18 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.send("API TETIFESS");
+  });
 app.use('/user', user)
+app.use('/posting', posting)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
-            console.log('Connected to MONGODB Server running on port ', process.env.PORT)
+            console.log('Server running on port ', process.env.PORT,'\nConnected to MongoDB...'
+            )
         })
     })
     .catch(err => console.log(err))
