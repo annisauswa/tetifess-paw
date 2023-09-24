5d38e9669a459a6eae6650d704b44f6aab16e01e
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-09-24 15:54:18 +0700

    refactor: change createUser to register

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,12 +1,18 @@
 const User = require('../model/user')
 
-const createUser = async (req, res) => {
+const registerUser = async (req, res) => {
     const { username, name, bio } = req.body
     try{
+        // check if username already taken
+        const checkUsername = await User.findOne({username})
+        if(checkUsername) {
+            return res.status(400).json({error: 'Username already taken'})
+        }
+
+        // registered successfully when username is awailable
         const user = await User.create({username, name, bio})
         res.status(200).json(user)
     } catch(err){
-        res.json({err})
         res.status(400).json({ error: err.message })
     }
 }
@@ -22,6 +28,6 @@ const getUser = async (req, res) => {
 }
 
 module.exports = {
-    createUser,
+    registerUser,
     getUser
 }
