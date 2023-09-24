cad80143f9bf7aeaa3fa142a86ebe8bd7fc7d84d
izzatarroyyan12 <izzatarroyyan12@gmail.com>
2023-09-24 16:54:57 +0700

    feat: add DELETE endpoint to delete user

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { createUser, getUser, updateUserBio } = require("../controller/user")
+const { createUser, getUser, updateUserBio, deleteUser } = require("../controller/user")
 
 router.get('/', getUser)
 router.post('/', createUser)
 router.put('/:userId', updateUserBio)
+router.delete('/:userId', deleteUser);
 
 module.exports = router;
