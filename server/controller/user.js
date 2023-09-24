5fa41293f510259448fa3ba196e15a6397215b27
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-09-24 16:16:51 +0700

    feat: register with password

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,7 +1,8 @@
 const User = require('../model/user')
 
 const registerUser = async (req, res) => {
-    const { username, name, bio } = req.body
+    const { username, password, name, bio } = req.body
+    const date_created = Date.now()
     try{
         // check if username already taken
         const checkUsername = await User.findOne({username})
@@ -10,7 +11,7 @@ const registerUser = async (req, res) => {
         }
 
         // registered successfully when username is awailable
-        const user = await User.create({username, name, bio})
+        const user = await User.create({username, password, name, bio, date_created})
         res.status(200).json(user)
     } catch(err){
         res.status(400).json({ error: err.message })
