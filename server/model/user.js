9be574f6b4960048dbf9d0ced80f4d7a7da8fe48
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-09-27 12:04:13 +0700

    feat: store postId in user likedPostings

--- server/model/user.js
+++ server/model/user.js
@@ -19,6 +19,10 @@ const userSchema = new Schema({
         type: String,
         required: false
     },
+    likedPostings: {
+        type: Array,
+        required: false
+    },
     dateCreated: {
         type: Date,
         required: true
