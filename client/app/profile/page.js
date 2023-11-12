f42c86f8322129e50ffa63ade4376ebfb7dab7d1
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-11-12 08:50:42 +0700

    feat: profile page

---
+++ client/app/profile/page.js
@@ -0,0 +1,22 @@
+'use client'
+import Homepage from '../../components/pages/homepage/Homepage'
+import Layout from '../../components/layout/Layout'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function Profile() {
+  const router = useRouter()
+  useEffect(() => {
+    if (!localStorage.getItem('token')) {
+      alert('You need to login first')
+      router.push('/login')
+    }
+  }, [])
+  return (
+    <Layout title='tetifess'>
+      <main className=' w-full h-screen text-black'>
+        <Homepage />
+      </main>
+    </Layout>
+  )
+}
