7fe2590c0f4fa9def7ee65d13df719c8a3426b4c
ethankuning <franciscusmarselino@gmail.com>
2023-12-02 15:27:14 +0700

    feat: repair button

--- client/components/element/Button.js
+++ client/components/element/Button.js
@@ -7,14 +7,14 @@ export function Button({
     isSubmit = "",
 }) {
   const buttonSize = useMemo(() => {
-      if (size === "sm") return "text-[14px] px-[24px] py-[13px] gap-2.5";
-      if (size === "md") return "text-[16px] px-[24px] py-[15px] gap-3.5";
-      if (size === "lg") return "text-[18px] px-[24px] py-[19px] gap-4";
-      return "text-[14px] px-[24px] py-[13px] gap-2.5";
+      if (size === "sm") return "text-[12px] w-16 h-6";
+      if (size === "md") return "text-[20px] w-60 h-12";
+      if (size === "lg") return "text-[20px] w-70 h-12";
+      return "text-[12px] w-16 h-6";
   }, [size]);
   return (
     <button 
-      className={`w-full bg-main hover:ring-[2px] hover:ring-main hover:bg-white text-white hover:text-main font-bold rounded-[24px] ${buttonSize}`}
+      className={`bg-main hover:ring-[2px] hover:ring-main hover:bg-white text-white hover:text-main font-bold rounded-[24px] ${buttonSize}`}
       type={isSubmit}>
       {text}
     </button>
