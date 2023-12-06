a41aab2b887ba7e77a9057b6637828079b34da8f
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-12-06 19:59:19 +0700

    fix: change button width height

--- client/components/element/Button.js
+++ client/components/element/Button.js
@@ -2,20 +2,21 @@ import { type } from "os";
 import { useMemo } from "react"
 
 export function Button({
+  width = "w-full",
   size = "sm",
   text = "Button",
   isSubmit = "",
-  onClick,  // Add this line
+  onClick,
 }) { 
 const buttonSize = useMemo(() => {
-    if (size === "sm") return "text-[12px] w-16 h-6";
-    if (size === "md") return "text-[20px] w-60 h-12";
-    if (size === "lg") return "text-[20px] w-70 h-12";
-    return "text-[12px] w-16 h-6";
+    if (size === "sm") return "text-[14px] py-[5px] rounded-[12px]";
+    if (size === "md") return "text-[16px] py-[8px] rounded-[16px]";
+    if (size === "lg") return "text-[20px] py-[10px] rounded-[24px]";
+    return "text-[12px] px-[10px] h-6";
 }, [size]);
   return (
     <button 
-      className={`bg-main hover:ring-[2px] hover:ring-main hover:bg-white text-white hover:text-main font-semibold rounded-[24px] ${buttonSize}`}
+      className={`bg-main hover:ring-[2px] hover:ring-main hover:bg-white text-white hover:text-main font-semibold rounded-[24px] ${buttonSize} ${width}`}
       type={isSubmit ? "submit" : "button"}  // Modify this line
       onClick={onClick}  // And this line
     >
