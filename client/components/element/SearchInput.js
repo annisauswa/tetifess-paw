13b1780579cb67e08181f97ea560643d8ee18fa8
ethankuning <franciscusmarselino@gmail.com>
2023-12-07 11:54:00 +0700

    fix: fix searchinput responsiveness

--- client/components/element/SearchInput.js
+++ client/components/element/SearchInput.js
@@ -2,16 +2,16 @@ import { MdSearch } from 'react-icons/md'
 
 function SearchInput({ label, id, placeholder, setValue, value }) {
   return (
-    <div className="font-dm-sans relative mb-4 w-full">
+    <div className="font-dm-sans mb-4 w-full">
       <label className="font-regular mb-0.5 ml-2 block font-roboto text-sm text-black">
         {label}
       </label>
-      <div className="relative">
-        <div className="absolute left-0 top-0 cursor-pointer px-3 py-2 text-black">
+      <div className="flex items-center">
+        {/* <div className="px-3 py-2 text-black">
           <MdSearch className="text-main" size={36} />
-        </div>
+        </div> */}
         <input
-          className="w-full rounded-[8px] border-[1px] border-tertiery px-3 py-4 pl-[60px] leading-tight text-black shadow"
+          className="w-full rounded-[8px] border-[1px] border-tertiery px-3 py-4 leading-tight text-black shadow"
           id={id}
           value={value}
           type="text"
