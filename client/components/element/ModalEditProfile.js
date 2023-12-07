d47b49f22e71b86af752c4a136e8a8f81a8adf22
Rexiar <raditya.cbp@gmail.com>
2023-12-07 11:39:57 +0700

    fix: dashboard edit profile

--- client/components/element/ModalEditProfile.js
+++ client/components/element/ModalEditProfile.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
 import { useEffect, useState } from 'react'
-import ReactDOM from 'react-dom'
 import { RiCloseFill } from 'react-icons/ri'
+import ReactDOM from 'react-dom'
 import { toast } from 'react-toastify'
+import axios from 'axios'
 
 export default function ModalEditProfile({ show, setShow, data }) {
   const role = localStorage.getItem('role')
@@ -24,13 +24,8 @@ export default function ModalEditProfile({ show, setShow, data }) {
         headers: {
           'Authorization': `Bearer ${localStorage.getItem('token')}`
         },
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-          withCredentials: true,
-        },
-      )
+        withCredentials: true
+      })
       .then(() => {
         toast.success('Profile updated')
         setShow(false)
@@ -42,68 +37,70 @@ export default function ModalEditProfile({ show, setShow, data }) {
       })
   }
 
-  useEffect(() => {}, [name, bio, username])
+  useEffect(() => {
+   
+  },[name, bio, username])
 
-  if (show === false) return null
+  if (show===false) return null
 
   return ReactDOM.createPortal(
+    (
     <div
-      id="wrapper"
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/25 text-black backdrop-blur-sm"
-    >
-      <div className="w-[500px]">
-        <div className="space-y-3 rounded-xl bg-secondary p-2 px-5">
-          <div className="flex justify-between">
-            <p className="font-leckerli-one text-xl font-bold">Edit Profile</p>
-            <button onClick={() => setShow(false)} className="px-2 text-4xl">
-              <RiCloseFill />
+      id='wrapper'
+      className='fixed inset-0 bg-black/25 backdrop-blur-sm flex justify-center items-center z-50 text-black'>
+      <div className='w-[500px]'>
+        <div className='bg-secondary p-2 rounded-xl space-y-3 px-5'>
+          <div className='flex justify-between'>
+            <p className='font-leckerli-one text-xl font-bold'>Edit Profile</p>
+            <button
+              onClick={() => setShow(false)}
+              className='text-4xl px-2'>
+                <RiCloseFill />
             </button>
           </div>
-          <div className="line" />
-          <div className="h-24 w-24 rounded-full bg-gray-400" />
-          <form className="flex flex-col">
-            <label className="text-sm font-semibold">Username</label>
+          <div className='line'></div>
+          <div className='w-24 h-24 bg-gray-400 rounded-full'></div>
+          <form className='flex flex-col'>
+            <label className='text-sm font-semibold'>Username</label>
             <input
-              type="text"
-              id="username"
-              name="username"
-              placeholder="e.g."
+              type='text'
+              id='username'
+              name='username'
+              placeholder='e.g.'
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="mb-5 rounded-md border border-tertiery bg-transparent px-2 py-2 focus:outline-none focus:ring-main"
+              className='px-2 py-2 mb-5 bg-transparent border rounded-md border-tertiery focus:ring-main focus:outline-none'
             />
-            <label className="text-sm font-semibold">Name</label>
+            <label className='text-sm font-semibold'>Name</label>
             <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="e.g."
+              type='text'
+              id='name'
+              name='name'
+              placeholder='e.g.'
               value={name}
               onChange={(e) => setName(e.target.value)}
-              className="mb-5 rounded-md border border-tertiery bg-transparent px-2 py-2 focus:outline-none focus:ring-main"
+              className='px-2 py-2 mb-5 bg-transparent border rounded-md border-tertiery focus:ring-main focus:outline-none'
             />
-            <label className="text-sm font-semibold">Bio</label>
+            <label className='text-sm font-semibold'>Bio</label>
             <textarea
-              id="bio"
-              name="bio"
-              placeholder="e.g.k"
+              id='bio'
+              name='bio'
+              placeholder='e.g.k'
               value={bio}
               onChange={(e) => setBio(e.target.value)}
-              className="mb-5 rounded-md border border-tertiery bg-transparent px-2 py-2 focus:outline-none focus:ring-main"
-            />
+              className='px-2 py-2 mb-5 bg-transparent border rounded-md border-tertiery focus:ring-main focus:outline-none'>
+            </textarea>
           </form>
-          <div className="line" />
-          <div className="flex justify-end">
-            <button
-              onClick={() => handleEdit()}
-              className="gap-2.5 rounded-[24px] bg-main px-[28px] py-[7px] text-[14px] font-bold   text-white hover:bg-white hover:text-main hover:ring-[2px] hover:ring-main"
-            >
+          <div className='line'></div>
+          <div className='flex justify-end'>
+            <button onClick={()=>handleEdit()} className={`bg-main hover:ring-[2px] hover:ring-main hover:bg-white text-white hover:text-main font-bold   rounded-[24px] text-[14px] px-[28px] py-[7px] gap-2.5`}>
               Save
             </button>
           </div>
         </div>
       </div>
-    </div>,
-    document.body,
-  )
+    </div>
+  ),
+  document.body
+);
 }
